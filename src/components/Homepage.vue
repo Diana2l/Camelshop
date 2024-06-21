9b44990f15bf9870edbe3f6cea0084b9a36f3414
Diana2l <diana.cheptoo.j@gmail.com>
2024-06-21 08:40:22 +0300

    changes

--- src/components/Homepage.vue
+++ src/components/Homepage.vue
@@ -35,28 +35,84 @@
          </script>
 
 <template>
-  <v-card
+  <v-card>
     class="mx-auto"
-    width="400"
+    width="1000"
   >
 
     <v-carousel>
   <v-carousel-item
-    src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+    src="public/Images/vinicius-amnx-amano-dbOV1qSiL-c-unsplash.jpg"
     cover
   ></v-carousel-item>
 
   <v-carousel-item
-    src="https://cdn.vuetifyjs.com/images/cards/hotel.jpg"
+    src="public/Images/customerbox-aXq1oCCjlVM-unsplash.jpg"
     cover
   ></v-carousel-item>
 
   <v-carousel-item
-    src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
-    cover
-  ></v-carousel-item>
+    src='../Images/jamie-albright-AHF_ZktTL6Q-unsplash.jpg'
+    cover>
+  </v-carousel-item>
 </v-carousel>
     </v-card>
+    <v-card align="center">
+      <v-card-item>
+      <v-card-title>Candy Shop</v-card-title>
+
+      <v-card-subtitle>The Best Candy Shop</v-card-subtitle>
+      </v-card-item>
+
+      <v-card-text></v-card-text>
+
+    </v-card>
+
+ <v-row>
+    <v-col cols="12" md="4">
+      <v-card
+        subtitle="This is a card subtitle"
+        text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, ratione debitis quis est labore voluptatibus!"
+        title="This is a title"
+      ></v-card>
+
+      <div class="text-center text-caption">Using Props Only</div>
+    </v-col>
+
+    <v-col cols="12" md="4">
+      <v-card>
+        <template v-slot:title>
+          This is a title
+        </template>
+
+        <template v-slot:subtitle>
+          This is a card subtitle
+        </template>
+
+        <template v-slot:text>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, ratione debitis quis est labore voluptatibus!
+        </template>
+      </v-card>
+
+      <div class="text-center text-caption">Using Slots Only</div>
+    </v-col>
+
+    <v-col cols="12" md="4">
+      <v-card>
+        <v-card-item>
+          <v-card-title>This is a title</v-card-title>
+
+          <v-card-subtitle>This is a card subtitle</v-card-subtitle>
+        </v-card-item>
+
+        <v-card-text>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, ratione debitis quis est labore voluptatibus!
+        </v-card-text>
+      </v-card>
+
+      <div class="text-center text-caption">Using Markup Only</div>
+    </v-col>
+  </v-row>
       
     </template>
     
